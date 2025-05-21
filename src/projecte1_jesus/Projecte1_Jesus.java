474a8703ddcd3fadeb259883eaee7dbb298b8170
Cisco-23 <adam23jcastro@inslaferreria.cat>
2025-05-21 20:16:30 +0200

    Adeu afegit

---
+++ src/projecte1_jesus/Projecte1_Jesus.java
@@ -0,0 +1,13 @@
+
+package projecte1_jesus;
+
+public class Projecte1_Jesus {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Adeu");
+       
+    }
+    
+}
