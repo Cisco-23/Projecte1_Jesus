67c7bb940a891e00877781954a874dcd132952a1
Cisco-23 <adam23jcastro@inslaferreria.cat>
2025-05-22 18:56:36 +0200

    mejorado

--- src/projecte1_jesus/Projecte1_Jesus.java
+++ src/projecte1_jesus/Projecte1_Jesus.java
@@ -7,7 +7,7 @@ public class Projecte1_Jesus {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Adeu");
-       
+        System.out.println("Hola como vas");
     }
     
 }
